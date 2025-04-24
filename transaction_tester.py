c421dd33b997798879bbd17b0e77492d6aedaa76
Xiang Yu Tuang <xiangyutuang@gmail.com>
2025-04-24 14:19:00 -0400

    more testing

--- transaction_tester.py
+++ transaction_tester.py
@@ -26,4 +26,30 @@ if __name__ == '__main__':
 
     transaction_bytes = transaction.to_bytes(True)
 
-    
+    res = transaction.verify()
+
+    if (res):
+        print("Valid transaction!") #Should print this line
+    else:
+        print("Invalid transaction!")
+
+
+
+    # Invalid case
+    invalid_private_key = private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    invalid_transaction = Transaction(public_key_bytes, time(), "voted for x")
+
+    invalid_transaction.sign(invalid_private_key)
+
+    res = invalid_transaction.verify()
+
+    if (res):
+        print("Valid transaction!")
+    else:
+        print("Invalid transaction!") # Should print this line
+
+
